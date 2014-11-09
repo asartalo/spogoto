708eb9c069b8c38b27cf71bce69980db867ff344
Wayne Duran <asartalo@gmail.com>
2014-11-10 02:41:49 +0800

    Adding interpreter tests and some new functions.

--- interpreter.go
+++ interpreter.go
@@ -4,6 +4,10 @@ type Interpreter struct {
 	DataStacks map[string]DataStack
 }
 
+func (i Interpreter) RegisterStack(name string, stack DataStack) {
+	i.DataStacks[name] = stack
+}
+
 func (i Interpreter) Stack(name string) DataStack {
 	s, ok := i.DataStacks[name]
 	if !ok {
@@ -11,4 +15,16 @@ func (i Interpreter) Stack(name string) DataStack {
 	}
 
 	return s
+}
+
+func (i Interpreter) Ok(name string, count int64) bool {
+	return !i.Bad(name, count)
+}
+
+func (i Interpreter) Bad(name string, count int64) bool {
+	return i.Stack(name).Size() < count
+}
+
+func NewInterpreter() Interpreter {
+	return Interpreter{make(map[string]DataStack)}
 }
---
+++ interpreter_test.go
@@ -0,0 +1,50 @@
+package spogoto
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestInterpreter(t *testing.T) {
+	Convey("Given an interpreter", t, func() {
+		i := NewInterpreter()
+		d1 := NewDataStack(Elements{1, 2}, FunctionMap{})
+		// d2 := NewDataStack(Elements{}, FunctionMap{})
+		i.RegisterStack("foo", d1)
+		// i.RegisterStack("bar", d2)
+
+		Convey("DataStacks are retrievable by name", func() {
+			So(i.Stack("foo"), ShouldEqual, d1)
+		})
+
+		Convey("When Ok() is called", func() {
+			Convey("On a non-existent stack should return false", func() {
+				So(i.Ok("bar", 2), ShouldBeFalse)
+			})
+
+			Convey("On a stack that does not meet the number of elements should return false", func() {
+				So(i.Ok("foo", 3), ShouldBeFalse)
+			})
+
+			Convey("On a stack that does meet the number of elements should return true", func() {
+				So(i.Ok("foo", 2), ShouldBeTrue)
+				So(i.Ok("foo", 1), ShouldBeTrue)
+			})
+		})
+
+		Convey("When Bad() is called", func() {
+			Convey("On a non-existent stack should return true", func() {
+				So(i.Bad("bar", 2), ShouldBeTrue)
+			})
+
+			Convey("On a stack that does not meet the number of elements should return true", func() {
+				So(i.Bad("foo", 3), ShouldBeTrue)
+			})
+
+			Convey("On a stack that does meet the number of elements should return false", func() {
+				So(i.Bad("foo", 2), ShouldBeFalse)
+				So(i.Bad("foo", 1), ShouldBeFalse)
+			})
+		})
+	})
+}
