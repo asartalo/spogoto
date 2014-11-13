1b7b57ac5eb46b9b03cad9442d02ca74a7543ce4
Wayne Duran <asartalo@gmail.com>
2014-11-13 12:22:04 +0800

    Adding test messaging helper functions for better convey messages.

--- test_helpers.go
+++ test_helpers.go
@@ -1,5 +1,9 @@
 package spogoto
 
+import (
+	"fmt"
+)
+
 type spogotoTestItem struct {
 	fn           string
 	intsBefore   []int64
@@ -34,4 +38,54 @@ func boolElements(bools []bool) Elements {
 		elements = append(elements, bool(v))
 	}
 	return elements
+}
+
+func tbooleansOnly(fn string, bs1 []bool, bs2 []bool) spogotoTestItem {
+	return spogotoTestItem{
+		fn,
+		[]int64{}, []int64{},
+		bs1, bs2,
+		[]float64{}, []float64{},
+	}
+}
+
+func tnothingHappens(fn string) spogotoTestItem {
+	return tbooleansOnly(fn, []bool{}, []bool{})
+}
+
+func tBoolPrimaryMessaging(d spogotoTestItem) (message string) {
+	message = fmt.Sprintf("Call()ing '%s' with boolean stack %v", d.fn, d.boolsBefore)
+	if len(d.intsBefore) > 0 {
+		message += fmt.Sprintf(" with integer stack %v", d.intsBefore)
+	}
+	if len(d.floatsBefore) > 0 {
+		message += fmt.Sprintf(" with float stack %v", d.floatsBefore)
+	}
+	return message
+}
+
+func tIntMessaging(before []int64, after []int64) (message string) {
+	if len(before) == 0 && len(after) == 0 {
+		message = "It shouldn't modify the integer stack"
+	} else {
+		message = fmt.Sprintf(
+			"It should modify the integer stack from %v to %v",
+			before, after,
+		)
+	}
+
+	return message
+}
+
+func tFloatMessaging(before []float64, after []float64) (message string) {
+	if len(before) == 0 && len(after) == 0 {
+		message = "It shouldn't modify the float stack"
+	} else {
+		message = fmt.Sprintf(
+			"It should modify the float stack from %v to %v",
+			before, after,
+		)
+	}
+
+	return message
 }
