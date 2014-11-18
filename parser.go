9e3ea5c4526dec844608c12e1f825f6012a0ff2b
Wayne Duran <asartalo@gmail.com>
2014-11-18 08:40:29 +0800

    Parser complete -- but using regexp. Don't have time to create something better. Interface allows this to be better.

--- parser.go
+++ parser.go
@@ -1,8 +1,8 @@
 package spogoto
 
 import (
-	"fmt"
 	"regexp"
+	"strings"
 )
 
 type Instruction struct {
@@ -12,33 +12,53 @@ type Instruction struct {
 }
 
 type Parser struct {
-	Functions map[string][]string
+	Functions map[string]map[string]bool
+}
+
+func (p *Parser) FunctionRegistered(t string, fn string) bool {
+	funcs, ok := p.Functions[t]
+	if ok {
+		return funcs[fn]
+	}
+	return false
 }
 
 func (p *Parser) Parse(code string) []Instruction {
 	re := regexp.MustCompile("\\s+")
 	raw := re.Split(code, -1)
 	i := []Instruction{}
-	fmt.Println(raw)
 	for _, item := range raw {
-		i = append(i, p.ParseItem(item))
+		parsed := p.ParseItem(item)
+		if parsed.Type != "" {
+			i = append(i, parsed)
+		}
 	}
 	return i
 }
 
 func (p *Parser) ParseItem(item string) Instruction {
 	var t string
+	var fn string
 	if item == "true" || item == "false" {
 		t = "boolean"
 	} else if regexp.MustCompile(`^\-?\d+$`).MatchString(item) {
 		t = "integer"
 	} else if regexp.MustCompile(`^\-?\d+\.\d+$`).MatchString(item) {
 		t = "float"
+	} else if regexp.MustCompile(`^\w+\.[^\.]+$`).MatchString(item) {
+		s := strings.Split(item, ".")
+		t = s[0]
+		fn = s[1]
+		if !p.FunctionRegistered(t, fn) {
+			t = ""
+			fn = ""
+		}
+
 	}
 
-	return Instruction{t, item, ""}
+	return Instruction{t, item, fn}
 }
 
 func NewParser() *Parser {
-	return &Parser{make(map[string][]string)}
+	return &Parser{make(map[string]map[string]bool)}
 }
--- parser_test.go
+++ parser_test.go
@@ -8,12 +8,9 @@ import (
 func TestParser(t *testing.T) {
 	Convey("Given a parser", t, func() {
 		parser := NewParser()
-		parser.Functions["integer"] = []string{"foo", "bar"}
-		parser.Functions["boolean"] = []string{"+", "-"}
-		parser.Functions["float"] = []string{"pizza", "pasta"}
+		parser.Functions["foo"] = map[string]bool{"bar": true, "baz": true, "+": true}
 
 		Convey("It can parse integer literals", func() {
-
 			code := "1 98 -27"
 			So(
 				parser.Parse(code), ShouldResemble,
@@ -26,7 +23,6 @@ func TestParser(t *testing.T) {
 		})
 
 		Convey("It can parse float literals", func() {
-
 			code := "0.528 3.14 -0.189"
 			So(
 				parser.Parse(code), ShouldResemble,
@@ -39,13 +35,34 @@ func TestParser(t *testing.T) {
 		})
 
 		Convey("It can parse boolean literals", func() {
-
 			code := "true false"
 			So(
 				parser.Parse(code), ShouldResemble,
 				[]Instruction{
 					Instruction{"boolean", "true", ""},
 					Instruction{"boolean", "false", ""},
+				},
+			)
+		})
+
+		Convey("It can parse known data stack functions", func() {
+			code := "foo.bar foo.baz foo.+"
+			So(
+				parser.Parse(code), ShouldResemble,
+				[]Instruction{
+					Instruction{"foo", "foo.bar", "bar"},
+					Instruction{"foo", "foo.baz", "baz"},
+					Instruction{"foo", "foo.+", "+"},
+				},
+			)
+		})
+
+		Convey("It will ignore unknown data stacks and functions", func() {
+			code := "foo.- fool.barz foo.bar"
+			So(
+				parser.Parse(code), ShouldResemble,
+				[]Instruction{
+					Instruction{"foo", "foo.bar", "bar"},
 				},
 			)
 		})
